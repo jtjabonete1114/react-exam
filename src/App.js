bbf1ad0c4aa1a2b9fc1eb831828f1e714b711fc9
kyben <kyben.developer@gmail.com>
2019-08-30 17:04:33 +0800

    Change sending of visit to fetch

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { useMutation } from '@apollo/react-hooks';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import VisitorPage from './pages/VisitorPageFunctional';
 import MainNavigation from './components/Navigation/MainNavigation';
 import { isMobile } from './Utilities/detect-device';
-import { createVisit } from './graphql/visits';
 
 const App = () => {
-  // const [fetchVisit] = useMutation(createVisit);
+  
+  useEffect(() => {
 
-  // const [isPageLoad, setSetPageNo] = useState(false);
+    let device = 'Desktop';
+    if (isMobile.any()) {
+      device = isMobile.any()[0];
+    }
 
-  // useEffect(() => {
-  //   if (!isPageLoad) {
-  //     let device = 'Desktop';
-  //     if (isMobile.any()) {
-  //       device = isMobile.any()[0];
-  //     }
-  //     fetch('http://www.geoplugin.net/json.gp', { method: 'GET' })
-  //     .then(req => {
-  //       return req.json();
-  //     })
-  //       .then(result => {
-  //         const variables ={
-  //           ip:result.geoplugin_request,
-  //           device:device,
-  //         }
-  //         fetchVisit({ variables: variables })
-  //         setSetPageNo(true);
-  //       })
-  //       .catch(err => {
-  //         setSetPageNo(true);
-  //       });
-  //   }
-  // },[]);
+    fetch(
+      'http://www.geoplugin.net/json.gp',
+      { method: 'GET' }
+    ).then(req => {
+        return req.json();
+    }).then(result => {
+      const saveVisit = {
+
+          query: `mutation {
+            createVisit(ip: "${result.geoplugin_request}", device:"${device}"){
+              _id
+              ipAddress
+              device
+              createdAt
+              updatedAt
+            }
+          }`
+      };
+       
+      fetch('http://localhost:3003/api', {
+          method: 'POST',
+          body: JSON.stringify(saveVisit),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+      }).then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      }).then(resData => {
+        console.log(resData)
+      }).catch(err => {
+        console.log(err);
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  },[]);
 
 
   return (
